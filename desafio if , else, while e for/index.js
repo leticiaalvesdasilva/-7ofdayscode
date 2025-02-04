3c2ca7130e323d99df3753bea3f604ffb348a11c
Leticia <alvesleticia42@yahoo.com>
2025-02-04 16:45:13 -0300

    if , else , while , for

---
+++ desafio if , else, while e for/index.js
@@ -0,0 +1,69 @@
+let nome = prompt('Escreva um nome');
+let area = prompt('Digite uma área de programação');
+let msg = `Olá ${nome}, você cursa ${area}!`;
+
+alert(msg);
+
+let pergunta1 = prompt('Você gosta de Front-end ou Back-end, responda 1 para Front e 2 para Back');
+
+if(pergunta1 == 1){
+    alert('Front-end');
+}
+if(pergunta1 == 2 ){
+    alert('Back-end');
+}
+else{
+    return;
+}
+ alert(pergunta1);
+
+let resposta1 = prompt ('Você quer aprender React ou Vue?');
+let resposta2 = prompt ('Você quer aprender C# ou Java?');
+
+if(resposta1 == React){
+    alert('React!');
+}
+let reposta3 = prompt ('Então você gosta de JS? Resposta 1 para sim e 2 para não');
+
+while (resposta3 != 1 || 2){
+    return resposta3;
+}
+    if(resposta3 = 1){
+        alert('Que legal');
+    }
+    else {
+        alert('Okay');
+    }
+
+    if(resposta1 == Vue){
+    alert('Vue');
+}
+
+let resposta4= prompt(' Então vc gosta de Framewoks? Responda 1 para sim e 2 para não!');
+
+while (resposta4 != 1 || 2){
+    return resposta4;
+}
+
+if (resposta4 == 1){
+    alert('Show de bola');
+}
+else {
+    alert('Okay');
+}
+
+let pergunta2 = prompt('Gostaria de ser um fullstack? Respnda 1 para sim e 2 para não')
+
+for (let pergunta2 = 1; pergunta2 < 1 ;){
+    alert ('Fullstack');
+}
+
+
+
+
+
+
+
+
+
+// existe outras areas de atuação ? 
