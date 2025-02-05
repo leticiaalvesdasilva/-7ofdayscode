d8debf74dc0d5f4762fe25e411cd2afa48fe7d0a
Leticia <alvesleticia42@yahoo.com>
2025-02-05 18:00:01 -0300

    término, sem uso do for e while, preciso incrementar , fazendo um paralelo

--- desafio if , else, while e for/index.js
+++ desafio if , else, while e for/index.js
@@ -12,58 +12,80 @@ if(pergunta1 == 1){
 if(pergunta1 == 2 ){
     alert('Back-end');
 }
-else{
-    return;
-}
- alert(pergunta1);
 
-let resposta1 = prompt ('Você quer aprender React ou Vue?');
-let resposta2 = prompt ('Você quer aprender C# ou Java?');
+let pergunta2 = prompt ('Você quer aprender React ou Vue? responda 1 para react e 2 para vue');
 
-if(resposta1 == React){
+if(pergunta2 == 1){
     alert('React!');
+    
+} else
+if(pergunta2 == 2){
+    alert ("Vue");
+
 }
-let reposta3 = prompt ('Então você gosta de JS? Resposta 1 para sim e 2 para não');
+let pergunta3 = prompt('Gostaria de ser um fullstack? Respnda 1 para sim e 2 para não');
 
-while (resposta3 != 1 || 2){
-    return resposta3;
+if (pergunta3 == 1){
+    alert ('Segue firme');
 }
-    if(resposta3 = 1){
-        alert('Que legal');
-    }
-    else {
-        alert('Okay');
-    }
-
-    if(resposta1 == Vue){
-    alert('Vue');
-}
-
-let resposta4= prompt(' Então vc gosta de Framewoks? Responda 1 para sim e 2 para não!');
-
-while (resposta4 != 1 || 2){
-    return resposta4;
-}
-
-if (resposta4 == 1){
-    alert('Show de bola');
-}
-else {
-    alert('Okay');
-}
-
-let pergunta2 = prompt('Gostaria de ser um fullstack? Respnda 1 para sim e 2 para não')
-
-for (let pergunta2 = 1; pergunta2 < 1 ;){
-    alert ('Fullstack');
+else{
+    alert ('Tá legal tem outros caminhos pra seguir');
 }
 
 
 
 
+//     if(resposta1 == Vue){
+//     alert('Vue');
+// }
+
+// let resposta4= prompt(' Então vc gosta de Framewoks? Responda 1 para sim e 2 para não!');
+
+// while (resposta4 != 1 || 2){
+// }
+
+// if (resposta4 == 1){
+//     alert('Show de bola');
+// }
+// else {
+//     alert('Okay');
+// }
 
 
 
 
+
+
+
+
+
+
+// let resposta2 = prompt ('Você quer aprender C# ou Java? responda 1 para c# e 2 para java');
+
+// if(resposta2 == 1){
+//     alert('Parabéns')
+// }
+// if(resposta2 == 2){
+//     alert
+// }
+// else {
+//     alert('Okay');
+// }
 
 // existe outras areas de atuação ? 
+
+// let reposta3 = prompt ('Então você gosta de JS? Resposta 1 para sim e 2 para não');
+
+// if(resposta3 == 1){
+//     alert('Show de bola')
+// }
+// else{
+//     alert('Tá certo');
+// }
+
+//     if(resposta3 = 1){
+//         alert('Que legal');
+//     }
+//     else {
+//         alert('Okay');
+//     }
